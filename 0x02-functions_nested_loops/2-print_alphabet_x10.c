4f9136decaf7f7fd7771f93e68481101a2f30fbc
Method <gershommethod02@gmail.com>
2023-06-21 11:14:20 -0700

    nn

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_×10(void)
+{
+	char c;
+	int i;
+
+	i= 0;
+	while (i<10)
+	{
+		c='a';
+		while (ch <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}

517990895d98c0fe807027c636afc1267ac5c2d2
Method <gershommethod02@gmail.com>
2023-07-09 03:14:39 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*main - print number using putchar
+*
+*Return: Always 0 (Success)
+*
+**/
+int main(void)
+{
+	int m;
+
+	for (m = 48 ; m < 58 ; m++)
+	putchar(m);
+	putchar('\n');
+
+	return (0);
+}

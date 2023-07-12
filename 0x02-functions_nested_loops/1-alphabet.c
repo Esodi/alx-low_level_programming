c3d4d8fdb471331d406db5e31799e55587dac494
Method <gershommethod02@gmail.com>
2023-07-11 23:29:56 -0700

    print_alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - prints lowcase alphabet
  *
  * Return: Always 0.
  */
 void print_alphabet(void)
 {
 	char l;
+
 	for (l = 'a'; l <= 'z'; l++)
 		_putchar(l);
 	_putchar('\n');

f1412e91ffe03dae6848a44b4dd52ed39c649c0e
Method <gershommethod02@gmail.com>
2023-07-15 09:08:09 -0700

    cc

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,11 +15,24 @@ void times_table(void)
 		r = 0;
 		while (r < 10)
 		{
-			_putchar(((c * r) / 10) + '0');
-			_putchar(((c * r) % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
+			if (r  == 0)
+			{
+				_putchar(r + '0');
+			}
+			if ((r * c) < 10 && r != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + (c * r));
+			}
+			else if ((c * r) >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(((c * r) / 10) + '0');
+				_putchar(((c * r) % 10) + '0');
+			}
 			r++;
 		}
 		_putchar('\n');

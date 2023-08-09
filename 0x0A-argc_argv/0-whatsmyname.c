46f3c442a47efbfbffb3ff28d052dd4c509b5a4c
Egao <gershommethod02@gmail.com>
2023-08-09 13:34:26 -0700

    ff

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - big function
+ * @argc: argument count
+ * @argv[]: pointer to arg vector
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	argc = 0;
+	printf("%s\n",argv[argc]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - big func
+ * @argc: argument count
+ * Retur: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	
+	printf("%d\n",argc);
+
+	return (0);
+}

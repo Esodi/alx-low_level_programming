f6f20823bd7f4d19cb43a77c878ef886ac977f17
Egao <gershommethod02@gmail.com>
2023-08-12 03:37:08 -0700

    nana

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * create_array - returns a characters
+ * @size: size of array
+ * @c: a character value
+ * Return: 0 (Success)
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	unsigned int i;
+
+	a = malloc(size * sizeof(char));
+	i = 0;
+	while (i < size)
+	{
+		a[i] = c;
+		i++;
+	}
+	return a;
+}

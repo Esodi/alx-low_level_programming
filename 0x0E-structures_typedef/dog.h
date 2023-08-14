037c14acc5edbaff9729e2fe5cbe27753820edfa
Egao <gershommethod02@gmail.com>
2023-08-14 10:14:17 -0700

    added description on dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,26 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - contain detail of the dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: contain three basic details
+ */
 
-struct dog {
+struct dog
+{
+	/**
+	 * @name: name of the dog
+	 * @age: age of the dog
+	 * @owner: owner of the dog
+	 */
 	char *name;
 	float age;
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/*dog_t *new_dog(char *name, float age, char *owner);*/
-/*void free_dog(dog_t *d);*/
 
 #endif /*DOG_H*/

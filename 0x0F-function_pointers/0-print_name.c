516f6827b9c3cb23ae6732966ec1547379933622
Egao <gershommethod02@gmail.com>
2023-08-16 08:41:47 -0700

    task 0 done

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: pointer to a name assigned
+ * @f: function pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}

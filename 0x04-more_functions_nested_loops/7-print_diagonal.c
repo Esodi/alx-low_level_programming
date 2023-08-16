dca850c624040887cb12de78a55a1c3a263fdc05
Egao <gershommethod02@gmail.com>
2023-08-16 00:48:04 -0700

    task 7 completed

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,9 +16,11 @@ void print_diagonal(int n)
 		{
 			for (j = 1; j <= i; j++)
 			{
-				_putchar(' ');
+				if (j < i)
+					_putchar(' ');
+				else
+					_putchar(92);
 			}
-			_putchar(92);
 			_putchar('\n');
 		}
 	}

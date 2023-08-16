3d3285b3de99b629813fd7f40b073e7bde38e783
Egao <gershommethod02@gmail.com>
2023-08-16 14:56:46 -0700

    created some files for task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "calc.h"
+
+/**
+ * op_add - returns the sum of a and b
+ * @a: first integer
+ * @b: second integer
+ */
+int op_add(int a, int b)
+{
+	return ((a) + (b));
+}
+
+/**
+ * op_sub - returns the sum of a and b
+ * @a: first integer
+ * @b: second integer
+ */
+int op_sub(int a, int b)
+{
+	return ((a) - (b));
+}
+
+/**
+ * op_sub - returns the sub of a and b
+ * @a: first integer
+ * @b: second integer
+ */
+int op_mul(int a, int b)
+{
+	return ((a) * (b));
+}
+
+/**
+ * op_div - returns the div of a and b
+ * @a: first integer
+ * @b: second integer
+ */
+int op_div(int a, int b)
+{
+	return((a) / (b));
+}
+
+/**
+ * op_mod - returns the mod of a and b
+ * @a: first integer
+ * @b: second integer
+ */
+int op_mod(int a, int b)
+{
+	return ((a) % (b));
+}

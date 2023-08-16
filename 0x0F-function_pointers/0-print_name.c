573402722524bc6ac37dbfc2d53c00aa58e81c4d
Egao <gershommethod02@gmail.com>
2023-08-16 09:42:00 -0700

    new updates for task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 
 /**
  * print_name - function that prints a name
@@ -7,8 +8,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL)
-		return (NULL);
-
-	(*f)(name);
+	if (name != NULL)
+		(*f)(name);
 }

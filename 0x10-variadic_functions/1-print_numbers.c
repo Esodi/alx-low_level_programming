b39b057e05cae0a48936c9f83726560148183dcc
Egao <gershommethod02@gmail.com>
2023-08-17 10:40:43 -0700

    task 1 update

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -54,10 +54,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	int x;
 	va_list par;
-	
+
 	if (separator == NULL)
 	{
-		NULL;
+		return (NULL);
 	}
 	va_start(par, n);
 	for (i = 0; i < n; i++)

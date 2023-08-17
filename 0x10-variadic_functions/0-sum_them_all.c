36d41795b79684220d3c1294dd6120adf2d4e507
Egao <gershommethod02@gmail.com>
2023-08-17 05:31:08 -0700

    task 0 was added

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - return the sum of its parameters
+ * @n; number of parameters
+ * Return: int value
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, x, sum = 0;
+	va_list par;
+
+	if (n == 0)
+		return (0);
+
+	va_start(par, n);
+
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(par, int);
+		sum = sum + x;
+	}
+	va_end(par);
+	return (sum);
+}

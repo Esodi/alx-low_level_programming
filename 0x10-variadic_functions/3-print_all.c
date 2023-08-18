f4ecd21388543c81204beb7a3bef7387b661f7f3
Egao <gershommethod02@gmail.com>
2023-08-18 05:19:58 -0700

    task 3 was debbuged successfully

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,44 +7,44 @@
  */
 void print_all(const char * const format, ...)
 {
-	int x, n;
+	int x = 0;
 	va_list par;
 	ch q;
 	char *b = "";
 
 	va_start(par, format);
-	n = strlen(format);
-	x = 0;
-	while (x < n)
+	if (format)
 	{
-		switch (format[x])
+		while (format[x])
 		{
-			case 'c':
-				q.c = (char)va_arg(par, int);
-				printf("%c", q.c);
-				break;
-			case 'i':
-				q.i = va_arg(par, int);
-				printf("%d", q.i);
-				break;
-			case 'f':
-				q.f = (float)va_arg(par, double);
-				printf("%f", q.f);
-				break;
-			case 's':
-				q.s = va_arg(par, char*);
-				if (q.s == NULL)
-					q.s = "(nil)";
-				printf("%s", q.s);
-				break;
-			default:
-				b = ", ";
-				break;
+			switch (format[x])
+			{
+				case 'c':
+					q.c = (char)va_arg(par, int);
+					printf("%s%c", b, q.c);
+					break;
+				case 'i':
+					q.i = va_arg(par, int);
+					printf("%s%d", b, q.i);
+					break;
+				case 'f':
+					q.f = (float)va_arg(par, double);
+					printf("%s%f", b, q.f);
+					break;
+				case 's':
+					q.s = va_arg(par, char*);
+					if (q.s == NULL)
+						q.s = "(nil)";
+					printf("%s%s", b, q.s);
+					break;
+				default:
+					x++;
+					continue;
+			}
+			b = ", ";
+			x++;
 		}
-		if (x < n - 1)
-			printf("%s", b);
-		x++;
 	}
-	va_end(par);
 	printf("\n");
+	va_end(par);
 }

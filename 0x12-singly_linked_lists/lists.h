0e343c22e16fd93bdf944a0dfda67706a70e4276
Egao <gershommethod02@gmail.com>
2023-08-25 09:39:34 -0700

    Task 0 done

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,7 +6,6 @@
 #include <string.h>
 #include <stddef.h>
 
-size_t print_list(const list_t *h);
 
 /**
  * struct list_s - singly linked list
@@ -22,5 +21,7 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
+
+size_t print_list(const list_t *h);
 
 #endif /*LISTS_H*/

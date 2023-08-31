cfc9f30f66fc8d8a408846ec29ba6d683c88a027
Egao <gershommethod02@gmail.com>
2023-08-31 12:46:12 -0700

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * pw - gives back power of a number.
+ * @base: base.
+ * @exp: exponent.
+ * Return: r.
+ */
+int pw(int base, int exp)
+{
+	int r = 1, i;
+
+	for (i = 0;  i < exp; i++)
+	{
+		r = base * r;
+	}
+	return (r);
+}
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: pointer to char.
+ * Return: int value.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int p, n, k = 0;
+	int z = 8, i = 0;
+
+	while (*b != '\0')
+	{
+		if ((*b >= 'a' && *b <= 'z') || (*b >= 'A' && *b <= 'Z'))
+		{
+			return (NULL);
+		}
+		b++;
+	}
+	b--;
+	while (i < z && *b != '\0')
+	{
+		p = *b - '0';
+		n = p * pw(2, i);
+		k = k + n;
+		b--;
+		i++;
+	}
+	return (k);
+}

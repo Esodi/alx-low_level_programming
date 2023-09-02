57d667b975120daaf396e9410db70839345c460e
Egao <gershommethod02@gmail.com>
2023-09-02 05:26:55 -0700

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string.
+ * @dest: destination.
+ * @src: source.
+ * @n: bytes.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}

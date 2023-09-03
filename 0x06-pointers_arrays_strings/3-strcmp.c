e49ee3e759666b7106ad1dfda4bb9ae9a70e7eae
Egao <gershommethod02@gmail.com>
2023-09-03 11:01:35 -0700

    task 3 added

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,15 +8,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int str1 = (int)*s1;
-	int str2 = (int)*s2;
+	int i = 0;
 
-	while (*s1 != '\0' && *s2 != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (str1 != str2)
-			return (str1 - str2);
-		s1++;
-		s2++;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
 	}
 	return (0);
 }

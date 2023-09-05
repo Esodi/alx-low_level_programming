9ce2290c1c28007c9b778d235b319f97aca5a956
Egao <gershommethod02@gmail.com>
2023-09-05 03:19:15 -0700

    6th upload, task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (file == -1)
 	{
 		return (-1);

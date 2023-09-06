cf97714d9ef772cebc63c43a7a3c2d438728064e
Egao <gershommethod02@gmail.com>
2023-09-06 12:48:59 -0700

    1st upload task2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,21 +4,26 @@
  * add_node - function that returns the number of elements.
  * @head: pointer to a pointer
  * @str: A string.
+ * Return: new node.
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new, ln = 0;
+	list_t *new;
+	int ln = 0;
 
 	new = (list_t *)malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
 	new->str = strdup(str);
-	while (head != NULL)
+	if (new->str == NULL)
+		return (NULL);
+	while (str[ln])
 	{
 		ln++;
 	}
 	new->len = ln;
 	new->next = *head;
-
 	*head = new;
 
-	return(new);
+	return (new);
 }

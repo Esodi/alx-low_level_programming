6ddc15576fd43e9adae9faee84874e9ad6f28ee2
Egao <gershommethod02@gmail.com>
2023-09-06 01:24:28 -0700

    1st upload task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring.
+ * @s: string.
+ * @accept: string.
+ * Return: number of bytes in the initial segment of s.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0, len = 0, j;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{	
+			if (s[i] == accept[j])
+			{
+				len++;
+			}
+		}
+		i++;
+	}
+	return (len - 2);
+}
+

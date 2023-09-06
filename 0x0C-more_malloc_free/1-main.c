7eaf9daaa1ae6507236cbfe536437717de380e82
Egao <gershommethod02@gmail.com>
2023-09-06 06:24:33 -0700

    1st upload, task 1

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -10,8 +10,9 @@
 int main(void)
 {
     char *concat;
+    char *ptr = NULL;
 
-    concat = string_nconcat(NULL, "School !!!", 6);
+    concat = string_nconcat(ptr, "School !!!", 6);
     printf("%s\n", concat);
     free(concat);
     return (0);

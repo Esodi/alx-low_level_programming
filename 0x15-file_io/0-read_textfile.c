9ccdfefed9cde28a64ddc36c790077350f16e008
Method Mkoma <gershommethod02@gmail.com>
2023-10-30 12:39:42 -0700

    3rd commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-	file = open(filename, O_RDWR | O_CREAT | O_TRUNC);
+	file = open(filename, O_RDONLY);
 	if (file == -1)
 	{
 		perror("open");

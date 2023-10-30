55e51d17184f1e343ab605078b4d7e0d9398c546
Method Mkoma <gershommethod02@gmail.com>
2023-10-30 12:34:57 -0700

    2nd comit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - readstext file and prints it to the POSIX standard output.
  * @filename: the filename.
  * @letters: letters.
  * Return: printed text.
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-	file = open(filename, O_RDONLY);
+	file = open(filename, O_RDWR);
 	if (file == -1)
 	{
 		perror("open");

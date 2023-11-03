9981850e19a75686c380d90c02c8101bf946b52b
Method Mkoma <gershommethod02@gmail.com>
2023-11-02 22:47:31 -0700

    1st commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,28 +9,18 @@
 int create_file(const char *filename, char *text_content)
 {
 	int file;
-	char *memo;
-	ssize_t b_rd, b_wr;
+	ssize_t b_wr;
 
 	if (filename == NULL)
 		return (-1);
+	if (text_content == NULL)
+		text_content = "";
 	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
-	memo = malloc(sizeof(char) * strlen(text_content) + 1);
-	if (memo == NULL)
-		return (-1);
-	b_rd = read(file, memo, sizeof(memo));
-	if (b_rd == -1)
-	{
-		free(memo);
-		close(file);
-		return (-1);
-	}
-	b_wr = write(STDOUT_FILENO, memo, b_rd);
+	b_wr = write(file, text_content, strlen(text_content));
 	if (b_wr == -1)
 	{
-		free(memo);
 		close(file);
 		return (-1);
 	}

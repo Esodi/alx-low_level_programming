e7ac991d08411685672508ee10c9008db781b4b1
Method Mkoma <gershommethod02@gmail.com>
2023-12-12 12:32:30 +0300

    Task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - number of elements in a linked dlistint_t .
+ * @h: head.
+ * Return: the number of elements in a linked.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *tmp = h;
+	size_t c = 0;
+
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+		c++;
+	}
+	c++;
+	return (c);
+}

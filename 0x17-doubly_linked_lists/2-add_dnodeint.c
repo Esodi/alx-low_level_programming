12d9cfc75f6f15f0c36004de42b90f622c2e79bc
Method Mkoma <gershommethod02@gmail.com>
2023-12-12 18:55:14 +0300

    Task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,6 +17,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		exit(1);
 	}
 	new->n = n;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
 	new->next = *head;
 	*head = new; 
 

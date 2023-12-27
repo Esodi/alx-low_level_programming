003cad2fefaf936426db098a8ff5d025d6191516
Method Mkoma <gershommethod02@gmail.com>
2023-12-27 17:56:02 +0300

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ptr;
+	unsigned long int i;
 
 	ptr = malloc(sizeof(hash_table_t));
 	if (ptr == NULL)
@@ -15,7 +16,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	ptr->size = size;
-	ptr->array = NULL;
+	ptr->array = malloc(sizeof(ptr->size) * size);
+	if (ptr->array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		ptr->array[i] = NULL;
+	}
 
 	return (ptr);
 }

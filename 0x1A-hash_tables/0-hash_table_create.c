f5516b2b25e1be9b0d6508edf94ec0ebd4af5b56
Method Mkoma <gershommethod02@gmail.com>
2023-12-27 20:49:23 +0300

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * hash_table - function that creates a hash table.
+ * hash_table_create - function that creates a hash table.
  * @size: the size of the array.
  * Return: a pointer to the newly created hash table.
  */

539562c3db34380c36e713d53cf09ada57e9494a
Method Mkoma <gershommethod02@gmail.com>
2023-12-28 10:41:25 +0300

    Task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function that adds an element to the hash table.
+ * @ht: is the hash table you want to add or update the key/value to.
+ * @key: is the key.
+ * @value: is the value associated with the key.
+ * Returns: 1 if it succeeded, 0 otherwise.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int size, indx, code;
+	hash_node_t *new;
+
+	if (key == NULL)
+	{
+		return (0);
+	}
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+	size = ht->size;
+	code = hash_djb2((const unsigned char *)key);
+	indx = code % size;
+	new->key = strdup(key);
+	new->value = strdup(value);
+	if (ht->array[indx] == NULL)
+	{
+		ht->array[indx] = new;
+		new->next = NULL;
+		return (1);
+	}
+	new->next = ht->array[indx];
+	ht->array[indx] = new;
+
+	return (1);
+}

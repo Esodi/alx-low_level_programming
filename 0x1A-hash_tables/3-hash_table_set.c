e50e2f53339a9ac38edda955bb10c2d450ffd97b
Method Mkoma <gershommethod02@gmail.com>
2023-12-28 10:53:43 +0300

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -5,7 +5,7 @@
  * @ht: is the hash table you want to add or update the key/value to.
  * @key: is the key.
  * @value: is the value associated with the key.
- * Returns: 1 if it succeeded, 0 otherwise.
+ * Return: 1 if it succeeded, 0 otherwise.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {

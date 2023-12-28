8399f8a7f54a9041eb67357b764c70674883a2b3
Method Mkoma <gershommethod02@gmail.com>
2023-12-28 10:44:04 +0300

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int size, indx, code;
 	hash_node_t *new;
 
-	if (key == NULL)
+	if (key == NULL || ht == NULL)
 	{
 		return (0);
 	}

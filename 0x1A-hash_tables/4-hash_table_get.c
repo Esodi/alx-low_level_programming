db95f67b54644a22acca2a9cff797a4654f499b3
Method Mkoma <gershommethod02@gmail.com>
2023-12-29 17:01:39 +0300

    Task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,19 +8,21 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *node;
-	unsigned long int index;
+	unsigned long int ind, size;
+	hash_node_t *tmp;
+	char *value = NULL;
 
-	if (ht == NULL || key == NULL || *key == '\0')
-		return (NULL);
-
-	index = key_index((const unsigned char *)key, ht->size);
-	if (index >= ht->size)
-		return (NULL);
-
-	node = ht->array[index];
-	while (node && strcmp(node->key, key) != 0)
-		node = node->next;
-
-	return ((node == NULL) ? NULL : node->value);
+	size = ht->size;
+	ind = key_index((const unsigned char *)key, (unsigned long int)size);
+	tmp = ht->array[ind];
+	while (tmp->next)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			value = strdup(tmp->value);
+			return (value);
+		}
+		tmp = tmp->next;
+	}
+	return (value);
 }
